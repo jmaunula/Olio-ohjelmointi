24298008a432d49e2286347de4db207a2dbad11c
jmaunula <maunulajouni@gmail.com>
2025-01-09 16:07:45 +0200

    teht1 arvauspeli tehty valmiiksi

--- teht1/main.cpp
+++ teht1/main.cpp
@@ -1,9 +1,47 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+int game(int, int);
+
 int main()
 {
-    cout << "Hello World!" << endl;
+    int guesses = game(1, 20);
+    cout <<"Arvauksia meni: " << guesses << endl;
+
     return 0;
+}
+
+int game(int minNum, int maxNum)
+{
+    srand(time(nullptr));
+    int randomNumber = minNum + rand() % (maxNum - minNum +1);
+    int userInput = 0;
+    int guessCount = 1;
+
+    cout << "Arvaa satunnaisluku valilla " << minNum << "-" << maxNum << endl;
+    //cout << randomNumber << endl;
+
+    while(userInput != randomNumber)
+    {
+        cin >> userInput;
+        if(userInput == randomNumber)
+        {
+            cout << "Oikea numero" << endl;
+        }
+        if (userInput < randomNumber)
+        {
+            cout << "Luku on suurempi" << endl;
+            guessCount++;
+        }
+        if (userInput > randomNumber)
+        {
+            cout << "Luku on pienempi" << endl;
+            guessCount++;
+        }
+    }
+
+    return guessCount;
 }
